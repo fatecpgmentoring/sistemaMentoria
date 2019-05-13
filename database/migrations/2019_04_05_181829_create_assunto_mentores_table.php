742b7fdc9de299486466f73b125abe536c99fd6f
paulohgama <santosps1990@gmail.com>
2019-05-13 09:19:53 -0300

    Mudanças migration

--- database/migrations/2019_04_05_181829_create_assunto_mentores_table.php
+++ database/migrations/2019_04_05_181829_create_assunto_mentores_table.php
@@ -15,7 +15,7 @@ class CreateAssuntoMentoresTable extends Migration
     {
         Schema::create('tb_assunto_usuarios', function (Blueprint $table) {
             $table->increments('id_assunto_usuario');
-            $table->unsignedInteger('mentor_id_mentor');
+            $table->unsignedInteger('usuario_id_usuario');
             $table->unsignedInteger('assunto_id_assunto');
             $table->foreign('usuario_id_usuario')
                     ->references('id_usuario')
