ea4bb64cef92cb43bedac67c4232999d74c3375a
pdonnarumma <astronomysaturn1997@gmail.com>
2019-05-22 14:10:06 -0300

    .

--- resources/assets/vuejs/app.js
+++ resources/assets/vuejs/app.js
@@ -18,6 +18,22 @@ Vue.use(Snotify);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('consultant-list', require('./vuejsTarot/ConsultantList.vue').default);
 
+/* Painel-Mentor */
+Vue.component('chat-mentor', require('./painel-mentor/ChatMentor.vue').default);
+Vue.component('conexoes-mentorados', require('./painel-mentor/ConexoesMentorados.vue').default);
+Vue.component('listar-comentarios', require('./painel-mentor/ListarComentarios.vue').default);
+Vue.component('listar-conteudo', require('./painel-mentor/ListarConteudo.vue').default);
+
+/* Painel-Mentorado */
+Vue.component('chat-mentorado', require('./painel-mentorado/ChatMentorado.vue').default);
+Vue.component('conexoes-mentores', require('./painel-mentorado/ConexoesMentores.vue').default);
+Vue.component('listar-conteudo', require('./painel-mentorado/ListarConteudo.vue').default);
+Vue.component('mentores', require('./painel-mentorado/Mentores.vue').default);
+
+/* Painel-Site */
+Vue.component('all-mentores', require('./painel-site/AllMentores.vue').default);
+Vue.component('best-grades-mentores', require('./painel-site/BestGradesMentores.vue').default);
+
 
 const app = new Vue({
     el: '#vue-app'
