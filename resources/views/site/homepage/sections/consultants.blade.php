de4c3e61dbf508c6c3aace46e00f188a202a6b4f
paulohgama <santosps1990@gmail.com>
2019-05-22 17:20:49 -0300

    altera

--- resources/views/site/homepage/sections/consultants.blade.php
+++ resources/views/site/homepage/sections/consultants.blade.php
@@ -1,7 +1,6 @@
 <section id="consultants" class="mt-3">
          <div class="container">
             <h2 class="h2-title center-sprite">
-            <span class="spriting sprite-hanger2"></span>
             Fale agora <br> com um Mentor
             </h2>
             @php
